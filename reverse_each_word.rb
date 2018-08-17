6664690005d2fb49ee0741479d9ffa43dfa42ff3
Nicholas  Luong <nckluong@gmail.com>
2018-08-17 18:10:05 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(sentence)
-  arr = sentence.split(/ /)
-  newSentence = ""
-  arr.each do |word|
+  sentence.collect("").each do |word|
    newSentence += word.reverse 
     end 
   newSentence
