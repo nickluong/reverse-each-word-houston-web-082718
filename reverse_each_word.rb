d377311b4f9c45a495fa32e42f763d56e615ca9f
Nicholas  Luong <nckluong@gmail.com>
2018-08-17 18:07:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
 def reverse_each_word(sentence)
   arr = sentence.split(/ /)
   newSentence = ""
-  arr.each do |x|
-   newSentence += x.reverse 
+  arr.each do |word|
+   newSentence += word.reverse 
+   if(word.index != arr.size)
+   newSentence +=" "
     end 
   newSentence
 end 
